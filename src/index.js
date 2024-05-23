82d94d49935e5f8f85909bfc4f637a28512d1bcb
Uthaeus <romanlavery@gmail.com>
2024-05-23 16:59:35 -0600

    successfully using context to display data

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.scss';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import UserContextProvider from './store/user-context';
+import RecipesContextProvider from './store/recipes-context';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+        <RecipesContextProvider>
+          <App />
+        </RecipesContextProvider>
+    </UserContextProvider>
   </React.StrictMode>
 );
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+
+import { UserContext } from "../store/user-context";
 
 export default function Home() {
+    const { user, isAdmin } = useContext(UserContext);
+
     return (
         <div>
             <h1>Home</h1>
+
+            {user && (
+                <>
+                    <p>Welcome {user.name}</p>
+                    <p>You are {isAdmin ? "an admin" : "a user"}</p>
+                </>
+            )}
         </div>
     );
 }
