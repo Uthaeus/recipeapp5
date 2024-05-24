03a341407d60794ba53fb6951a32ee00cf510afd
Uthaeus <romanlavery@gmail.com>
2024-05-24 14:25:20 -0600

    added styling to homepage and sidebar

--- src/components/recipes/recipe-item.js
+++ src/components/recipes/recipe-item.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
+import image from '../../assets/images/spaghetti_image.png';
+
 export default function RecipeItem({ recipe }) {
 
     return (
         <div className="recipe-item">
-            <Link to={`/recipes/${recipe.id}`}>{recipe.name}</Link>
-            <p>{recipe.description}</p>
+            <img src={image} alt="spaghetti" style={{ width: '100%', maxHeight: '200px', objectFit: 'cover', objectPosition: 'center' }} />
+            <Link to={`/recipes/${recipe.id}`} className="recipe-item-title">{recipe.name}</Link>
+            <p className="recipe-item-description">{recipe.description}</p>
         </div>
     );
 }
--- src/components/sidebar/main-sidebar.js
+++ src/components/sidebar/main-sidebar.js
@@ -14,20 +14,36 @@ export default function MainSidebar() {
         <div className="main-sidebar">
             { user && <Link to="/recipes/new" className="new-recipe-link">Add New Recipe</Link>}
 
-            <h2>Categories</h2>
+            <h1 className="sidebar-title">Filter Recipes</h1>
 
-            <p onClick={() => filterRecipes('all')}>All</p>
-            <ul>
-                {categories.map(category => (
-                    <li key={category}>
-                        <p
-                            onClick={() => filterRecipes(category)}
-                        >
+            <div className="sidebar-container">
+                <h2 className="sidebar-subtitle">By Category</h2>
+
+                <p onClick={() => filterRecipes('all')} className="sidebar-reset-btn">All</p>
+
+                <div className="sidebar-categories-container">
+                    {categories.map(category => (
+                        <p key={category} className="sidebar-category" onClick={() => filterRecipes(category)}>
                             {category}
                         </p>
-                    </li>
-                ))}
-            </ul>
+                    ))}
+                </div>
+
+            </div>
+
+            <div className="sidebar-container">
+                <h2 className="sidebar-subtitle">By Time</h2>
+
+                <p className="sidebar-reset-btn">Reset Time Filter</p>
+
+                <select name="time" id="time" className="form-select sidebar-select">
+                    <option value="">select time</option>
+                    <option value="15">15 minutes</option>
+                    <option value="30">30 minutes</option>
+                    <option value="45">45 minutes</option>
+                    <option value="60">60 minutes</option>
+                </select>
+            </div>
         </div>
     );
 }
