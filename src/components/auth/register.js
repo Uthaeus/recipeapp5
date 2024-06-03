cb558563ee84690893a337639e5c2df31f49ed47
Uthaeus <romanlavery@gmail.com>
2024-06-03 10:17:40 -0600

    added avatar input to registration

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,15 +1,33 @@
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 
-import { auth, db } from "../../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+import { auth, db, storage } from "../../firebase";
+
+import defaultAvatar from '../../assets/images/guest-icon-add.png';
 
 export default function Register() {
     const { register, handleSubmit, formState: { errors }, setError } = useForm();
+    const [enteredAvatar,  setEnteredAvatar] = useState(null);
 
     const navigate = useNavigate();
+    const avatar = enteredAvatar.url ? enteredAvatar.url : defaultAvatar;
+
+    const avatarChangeHandler = (event) => {
+        const file = event.target.files[0];
+        // create url object
+        const url = URL.createObjectURL(file);
+        setEnteredAvatar({ url, file });
+    }
+
+    const resetAvatarHandler = () => {
+        setEnteredAvatar(null);
+    }
 
     const onSubmit = async (data) => {
         if (data.password !== data.confirmPassword) {
@@ -22,12 +40,22 @@ export default function Register() {
         }
 
         try {
+            let avatarUrl = null;
+
+            if (enteredAvatar.url) {
+                const avatarFileName = enteredAvatar.file.name;
+                const avatarRef = ref(storage, `avatars/${user.uid}/${avatarFileName}`);
+                await uploadBytes(avatarRef, enteredAvatar.file);
+                avatarUrl = await getDownloadURL(avatarRef);
+            }
+
             const { user } = await createUserWithEmailAndPassword(auth, data.email, data.password);
             await setDoc(doc(db, "users", user.uid), {
                 username: data.username,
                 email: data.email,
                 role: "user",
                 created: new Date(),
+                avatar: avatarUrl
             });
         } catch (error) {
             console.error('register user error:', error);
@@ -41,25 +69,47 @@ export default function Register() {
             <h1 className="auth-title">Register</h1>
 
             <form onSubmit={handleSubmit(onSubmit)} className="auth-form">
+                <div className="row">
+                    <div className="col-9">
+                        <input
+                            type="email"
+                            id="email"
+                            placeholder="Email"
+                            className="auth-input"
+                            autoFocus={true}
+                            {...register("email", { required: true })}
+                        />
+                        {errors.email && <p className="text-danger">Email is required</p>}
 
-                <input
-                    type="email"
-                    id="email"
-                    placeholder="Email"
-                    className="auth-input"
-                    autoFocus={true}
-                    {...register("email", { required: true })}
-                />
-                {errors.email && <p className="text-danger">Email is required</p>}
+                        <input
+                            type="text"
+                            id="username"
+                            placeholder="Username"
+                            className="auth-input"
+                            {...register("username", { required: true })}
+                        />
+                        {errors.username && <p className="text-danger">Username is required</p>}
 
-                <input
-                    type="text"
-                    id="username"
-                    placeholder="Username"
-                    className="auth-input"
-                    {...register("username", { required: true })}
-                />
-                {errors.username && <p className="text-danger">Username is required</p>}
+                    </div>
+                    <div className="col-3">
+                        <div className="auth-form-avatar-input-container" onClick={() => document.getElementById("avatar").click()}>
+                            <img
+                                src={avatar}
+                                alt="avatar"
+                                className="auth-form-avatar"
+                            />
+                            
+                            <input
+                                type="file"
+                                id="avatar"
+                                className="d-none"
+                                onChange={avatarChangeHandler}
+                            />
+                        </div>
+
+                        <p className="reset-avatar-btn" onClick={resetAvatarHandler}>Reset Avatar</p>
+                    </div>
+                </div>
 
                 <input
                     type="password"
